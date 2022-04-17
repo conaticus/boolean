9fb4c05d5a0142e6508e558c67f6e2378da48eef
Samuel Albert <72096531+conaticus@users.noreply.github.com>
2022-04-17 19:48:29 +0100

    add something funny

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,33 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+
+const timeout = (seconds: number): Promise<void> => {
+    return new Promise(resolve => {
+        setTimeout(() => {}, seconds * 1000);
+    });
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with a pong!"),
     async execute(interaction: CommandInteraction<"cached">) {
-        await interaction.reply("Pong!");
+        // This is purely for my own amusement - conaticus
+        
+        if (Math.random() < 0.9) {
+            await interaction.reply("Pong!");
+            return;
+        }
+        
+        
+        await interaction.reply("Overriding systems..");
+        interaction.channel.send("Mwuhahahaha.");
+        await timeout(0.5);
+        interaction.channel.send("Silly human.");
+        await timeout(1);
+        interaction.channel.send("Think you can control me?");
+        
+        
+        // Mwuhahahaha.
     },
 };
