f11aba41c051b2e024e6cf7555b79fc85af12005
gavin <92367661+beigeshibe@users.noreply.github.com>
2022-04-18 09:53:04 +1000

    Update ping.ts
    i controled the bot

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -13,18 +13,9 @@ module.exports = {
         .setDescription("Replies with a pong!"),
     async execute(interaction: CommandInteraction<"cached">) {
         // This is purely for my own amusement - conaticus
+        // infact i can control you
+        await interaction.reply("Pong!");
+        return;
         
-        if (Math.random() < 0.9) {
-            await interaction.reply("Pong!");
-            return;
-        }
-        
-        
-        await interaction.reply("Overriding systems..");
-        interaction.channel?.send("Mwuhahahaha.");
-        await timeout(0.5);
-        interaction.channel?.send("Silly human.");
-        await timeout(1);
-        interaction.channel?.send("Think you can control me?");
     },
 };
