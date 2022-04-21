7cc677b32a3a293b7c03299154cb7014ff24be79
InfinityDevTech <47366052+InfinityDevTech@users.noreply.github.com>
2022-04-21 11:13:18 -0400

    Fixed math command

--- src/commands/math.ts
+++ src/commands/math.ts
@@ -1,32 +1,11 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { MessageEmbed } from "discord.js";
-import { evaluate } from "mathjs";
 import { IBotCommand } from "../types";
 
 export const command: IBotCommand = {
     data: new SlashCommandBuilder()
         .setName("math")
-        .setDescription("Calculates the input.")
-        .addStringOption((option) =>
-            option
-                .setName("calculation")
-                .setDescription("Calculation to evaluate.")
-                .setRequired(true)
-        ),
+        .setDescription("Calculator function!")  
     async execute(interaction) {
-        const calc = interaction.options.getString("calculation", true);
-
-        try {
-            const result = evaluate(calc);
-            const successEmbed = new MessageEmbed()
-                .setColor("GREEN")
-                .setDescription(`Input: \`${calc}\`\nResult: \`${result}\``);
-            interaction.reply({ embeds: [successEmbed], ephemeral: true });
-        } catch (err) {
-            const errorEmbed = new MessageEmbed()
-                .setColor("RED")
-                .setDescription(`Input: \`${calc}\`\nError: \`${err}\``);
-            interaction.reply({ embeds: [errorEmbed], ephemeral: true });
-        }
+            interaction.reply("https://www.google.com/search?q=google+calculator");
     },
 };
