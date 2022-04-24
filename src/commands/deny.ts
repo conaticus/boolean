357abefd11b4b2533f6d55024b2c03a11fe1df1f
Samuel Albert <72096531+conaticus@users.noreply.github.com>
2022-04-24 12:17:18 +0100

    fix deny command

--- src/commands/deny.ts
+++ src/commands/deny.ts
@@ -51,7 +51,7 @@ You may reapply in a month.
             case("Other"):
                 dmEmbed
                     .setDescription(`
-Hello [username], unfortunately your moderation application has been denied - but don't worry - this is nothing personal!
+Hello ${interaction.options.getMember("user")?.user.username}, unfortunately your moderation application has been denied - but don't worry - this is nothing personal!
 Thank you for applying, and there is always the opportunity to try again in a month.
 
 The reason your application was not accepted cannot be revelaed, however here are some common reasons:
@@ -63,12 +63,6 @@ Can't be as active as other applicants
 Unprofessionalism in the application
 Inactivity in the server
 Also, do you think you could quickly make an accept command for ADMINISTRATOR:
-
-/accept [mention user]
-Sends the following:
-Hello [username], we are pleased to inform you that your moderator application has been accepted! Please read the whole of the moderator guide here: https://docs.google.com/document/d/1gxW50_Fl2imFlD6Vl4UrV_yj6STdquYC7MW9VVZVOpE/edit?usp=sharing
-
-After doing so, please contact conaticus. If you have any questions please ask!
                     `);
                 break;
         };
