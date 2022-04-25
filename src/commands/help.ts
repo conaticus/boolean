eb33038400d3ae5ba80f96608bdc001a96a040c8
aspectx <gamernxt6@gmail.com>
2022-04-25 08:40:50 +0700

    added help command

---
+++ src/commands/help.ts
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+
+import { commandFiles } from "../files";
+import { Bot } from "../structures/Bot";
+import { IBotCommand } from "../types/types";
+
+export const command: IBotCommand = {
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("A help embed to guide users to all the commands"),
+    async execute(interaction: CommandInteraction<"cached">, client: Bot) {
+        const embed = new MessageEmbed()
+            .setTitle("Help Embed")
+            .setColor("ORANGE")
+            .setTimestamp()
+            .setDescription(
+                "These are a list of all the commands on the server"
+            );
+
+        for await (const file of commandFiles) {
+            const command = (await import(file)).command as IBotCommand;
+            if (!command) {
+                console.error(
+                    `File at path ${file} seems to incorrectly be exporting a command.`
+                );
+                continue;
+            }
+            embed.addField(command.data.name, command.data.description, true);
+        }
+
+        interaction.reply({ embeds: [embed] });
+    },
+};
