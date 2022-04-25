5a12ea6b9ef1f87bb218e57d1e435260272b2622
aspectx <gamernxt6@gmail.com>
2022-04-25 12:00:06 +0700

    added user profile command

---
+++ src/commands/userProfile.ts
@@ -0,0 +1,75 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import moment from "moment";
+
+import { config_ as config } from "../configs/config-handler";
+import { commandFiles } from "../files";
+import { Bot } from "../structures/Bot";
+import { IBotCommand } from "../types/types";
+
+export const command: IBotCommand = {
+    data: new SlashCommandBuilder()
+        .setName("user")
+        .setDescription("get a user")
+        .addUserOption((op) =>
+            op
+                .setName("target")
+                .setDescription("the target user.")
+                .setRequired(true)
+        ),
+    async execute(interaction: CommandInteraction<"cached">, client: Bot) {
+        const target = interaction.options.getUser("target");
+        const guild = client.guilds.cache.get(config.guildId);
+        const member = guild?.members.cache.get(target?.id!);
+
+        let presenceIcon;
+        let presence;
+        if (member?.presence?.status === "dnd") {
+            presenceIcon = ":red_circle:";
+            presence = "Do Not Disturb";
+        } else if (member?.presence?.status === "idle") {
+            presenceIcon = ":yellow_circle:";
+            presence = "Idle";
+        } else if (member?.presence?.status === "online") {
+            presenceIcon = ":green_circle:";
+            presence = "Online";
+        } else if (
+            member?.presence?.status === "invisible" ||
+            member?.presence?.status === "offline"
+        ) {
+            presenceIcon = ":black_circle:";
+            presence = "Offline";
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`${target?.username}'s User Profile`)
+            .setThumbnail(`${target?.displayAvatarURL()}`)
+            .setTimestamp()
+            .setColor("ORANGE")
+            .addFields(
+                { name: `UserTag`, value: `${target?.tag}`, inline: true },
+                {
+                    name: `Created at`,
+                    value: ` <t:${Math.round(
+                        target?.createdTimestamp! / 1000
+                    )}:R>`,
+                    inline: true,
+                },
+                {
+                    name: `Joined at`,
+                    value: ` <t:${Math.round(
+                        member?.joinedTimestamp! / 1000
+                    )}:R>`,
+                    inline: true,
+                },
+                {
+                    name: `Status`,
+                    value: `${presenceIcon} ${presence}`,
+                    inline: true,
+                }
+            )
+            .setFooter({ text: `UserId: ${target?.id}` });
+
+        interaction.reply({ embeds: [embed] });
+    },
+};
