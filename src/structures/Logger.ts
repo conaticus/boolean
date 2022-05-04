9bde4a7c1e05f530962fe3dd5ea8ea41df1d17d8
Jakob <75086936+Jakob5358@users.noreply.github.com>
2022-05-04 17:17:50 +0200

    Make logger more ts-friendly & fix guildMemberAdd

--- src/structures/Logger.ts
+++ src/structures/Logger.ts
@@ -1,31 +1,17 @@
-import { Channel, MessageEmbed, TextChannel } from "discord.js";
+import { Channel, MessageEmbed } from "discord.js";
+import pino, { LoggerOptions, Logger as PinoLogger } from "pino";
 
-const pino = require("pino");
-/**
- * @class Logger
- */
 export default class Logger {
-    Wconsole: any;
-    /**
-     * @prop {Array} transports
-     * @prop {Boolean} exitOnError
-     */
-    constructor(options?: any) {
-        this.Wconsole = new pino({
+    console: PinoLogger;
+
+    constructor(options: LoggerOptions) {
+        this.console = pino({
             transport: { target: "pino-pretty" },
             ...options,
         });
     }
 
-    get console(): any {
-        return this.Wconsole;
-    }
-
     async channel(embed: MessageEmbed, channel: Channel) {
-        if (channel instanceof TextChannel) {
-            await channel.send({ embeds: [embed] });
-        }
+        if (channel.isText()) await channel.send({ embeds: [embed] });
     }
 }
-
-module.exports = Logger;
