609af7224a1713e8e82bd9068e84ef8f026a46af
xhayper <hayper1919@gmail.com>
2022-05-20 10:04:14 +0700

    ref(files): Switched over from filehound to fs

--- src/files.ts
+++ src/files.ts
@@ -1,22 +1,36 @@
 import path from "path";
+import fs from "fs";
 
-const filehound = require("filehound"); // sad
+const walk = (
+    pathLike: fs.PathLike,
+    options?:
+        | {
+              encoding: BufferEncoding | null;
+          }
+        | BufferEncoding
+        | null
+        | undefined
+): string[] => {
+    let results: string[] = [];
+    const fileList = fs.readdirSync(pathLike, options);
+    for (const file of fileList) {
+        const stat = fs.statSync(path.join(pathLike.toString(), file));
+        results = [
+            ...results,
+            ...(stat && stat.isDirectory()
+                ? walk(path.join(pathLike.toString(), file))
+                : [file]),
+        ];
+    }
+    return results;
+};
 
-function generator() {
-    return filehound
-        .create()
-        .depth(Infinity) // just for future-proofing in case of subdirectories
-        .ext(["ts", "js"]);
-}
+const commandFiles = walk(path.join(__dirname, "commands")).filter((file) =>
+    [".ts", ".js"].some((ext) => file.endsWith(ext))
+);
 
-const commandFiles = generator()
-    .path(path.join(__dirname, "commands"))
-    .findSync()
-    .map((file: string) => file.slice(0, file.lastIndexOf("."))) as string[];
-
-const eventFiles = generator()
-    .path(path.join(__dirname, "events"))
-    .findSync()
-    .map((file: string) => file.slice(0, file.lastIndexOf("."))) as string[];
+const eventFiles = walk(path.join(__dirname, "events")).filter((file) =>
+    [".ts", ".js"].some((ext) => file.endsWith(ext))
+);
 
 export { commandFiles, eventFiles };
