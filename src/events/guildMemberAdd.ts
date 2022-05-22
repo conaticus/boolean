df5660227299182492c606935f9d70c565aad352
Dylan <dylhack@outlook.com>
2022-05-22 08:25:44 +0000

    ref(guildMemberAdd): added rolemenu to welcome

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -19,7 +19,7 @@ export default TypedEvent({
             .setTitle("New Member")
             .setDescription(
                 `Welcome ${member.user.username} to the conaticus server\n` +
-                    "Use `/roleme` to choose your pings and languages roles\n" +
+                    "Use `/rolemenu` to choose your pings and languages roles\n" +
                     "Enjoy your stay!"
             );
 
