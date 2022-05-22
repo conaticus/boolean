ebf484340a2cf6090d67c9606f7483cb92e7d805
Dylan Hackworth <dylhack@outlook.com>
2022-05-22 16:44:15 -0500

    ref(verbal): added if guard

--- src/commands/verbal.ts
+++ src/commands/verbal.ts
@@ -73,88 +73,98 @@ class Verbal extends BotCommand {
         const dm = await member
             .send({ embeds: [dmEmbed], components })
             .catch(() => null);
-        if (dm) {
-            const collector = dm.createMessageComponentCollector({
-                componentType: "BUTTON",
-                time: 600_000,
+        const close = async () => {
+            await warnChannel.send({
+                embeds: [warnEmbed],
             });
-            collector.on("collect", async (i) => {
-                await i.showModal({
-                    customId: `appeal_${i.id}`,
-                    title: "Appeal warning",
-                    components: [
-                        {
-                            type: "ACTION_ROW",
-                            components: [
-                                {
-                                    type: "TEXT_INPUT",
-                                    label: "Elaborate",
-                                    placeholder:
-                                        "Explain why you think your warning was unjustified",
-                                    style: "PARAGRAPH",
-                                    customId: "content",
-                                    required: true,
-                                },
-                            ],
-                        },
-                    ],
-                });
-                const int = await i
-                    .awaitModalSubmit({
-                        filter: (inte) => inte.customId === `appeal_${i.id}`,
-                        time: 600_000,
-                    })
-                    .catch(() => null);
-                if (!int) {
-                    await i.followUp({
-                        content: "Modal timed out",
-                        ephemeral: true,
-                    });
-                    return;
-                }
-                const appealEmbed = new MessageEmbed()
-                    .setColor(0x2f3136)
-                    .setAuthor({
-                        name: `${member.user.username} appealed their warning`,
-                        iconURL: member.user.displayAvatarURL({
-                            dynamic: true,
-                        }),
-                    })
-                    .setDescription(int.fields.getTextInputValue("content"))
-                    .setTimestamp()
-                    .addField("Offender", member.toString(), true)
-                    .addField("Moderator", interaction.user.toString(), true)
-                    .addField("Warning reason", reason);
-                const optAppeal = await getSpecialChannel(
-                    interaction.guild.id,
-                    "appeals"
+
+            const successMessageEmbed = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(
+                    `Warning successfully issued at ${warnChannel}`
                 );
-                if (!optAppeal)
-                    throw new Error("There is not an appeals channel yet.");
-                const appealsChannel = optAppeal as TextChannel;
-                await appealsChannel.send({ embeds: [appealEmbed] });
-                appealButton.disabled = true;
-                await int.update({ components });
+
+            await interaction.reply({
+                embeds: [successMessageEmbed],
+                ephemeral: true,
             });
-            collector.on("end", async () => {
-                if (!dm.editable) return;
-                appealButton.disabled = true;
-                await dm.edit({ components });
-            });
+        };
+        if (!dm) {
+            await close();
+            return;
         }
-
-        await warnChannel.send({
-            embeds: [warnEmbed],
+        const collector = dm.createMessageComponentCollector({
+            componentType: "BUTTON",
+            time: 600_000,
         });
-
-        const successMessageEmbed = new MessageEmbed()
-            .setColor("GREEN")
-            .setDescription(`Warning successfully issued at ${warnChannel}`);
-
-        await interaction.reply({
-            embeds: [successMessageEmbed],
-            ephemeral: true,
+        collector.once("end", async () => {
+            if (!dm.editable) {
+                return;
+            }
+            appealButton.setDisabled(true);
+            await dm.edit({ components });
         });
+        collector.on("collect", async (i) => {
+            await i.showModal({
+                customId: `appeal_${i.id}`,
+                title: "Appeal warning",
+                components: [
+                    {
+                        type: "ACTION_ROW",
+                        components: [
+                            {
+                                type: "TEXT_INPUT",
+                                label: "Elaborate",
+                                placeholder:
+                                    "Explain why you think your warning was unjustified",
+                                style: "PARAGRAPH",
+                                customId: "content",
+                                required: true,
+                            },
+                        ],
+                    },
+                ],
+            });
+            const int = await i
+                .awaitModalSubmit({
+                    filter: (inte) => inte.customId === `appeal_${i.id}`,
+                    time: 600_000,
+                })
+                .catch(() => null);
+            if (!int) {
+                await i.followUp({
+                    content: "Modal timed out",
+                    ephemeral: true,
+                });
+                return;
+            }
+            const appealEmbed = new MessageEmbed()
+                .setColor(0x2f3136)
+                .setAuthor({
+                    name: `${member.user.username} appealed their warning`,
+                    iconURL: member.user.displayAvatarURL({
+                        dynamic: true,
+                    }),
+                })
+                .setDescription(int.fields.getTextInputValue("content"))
+                .setTimestamp()
+                .addField("Offender", member.toString(), true)
+                .addField("Moderator", interaction.user.toString(), true)
+                .addField("Warning reason", reason);
+            const optAppeal = await getSpecialChannel(
+                interaction.guild.id,
+                "appeals"
+            );
+            if (!optAppeal) {
+                throw new Error("There is not an appeals channel yet.");
+            }
+            const appealsChannel = optAppeal as TextChannel;
+            await appealsChannel.send({ embeds: [appealEmbed] });
+            appealButton.setDisabled(true);
+            await int.update({ components });
+            collector.stop();
+        });
+        await close();
     }
 }
 
