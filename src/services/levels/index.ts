e828aa20afd7bc1776085431bcdd7bdd53b17b19
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-06-03 10:37:39 +0800

    add rank command stub

--- src/services/levels/index.ts
+++ src/services/levels/index.ts
@@ -1,4 +1,6 @@
 import { isLevelCooldown, upsertLevelEntry } from "./database";
+import { BotCommand } from "../../structures";
+import RankCommand from "./commands/rank";
 
 // TODO(HTG-YT): implement ratelimiting for EXP gaining
 export async function updateLevels(memberId: string): Promise<void> {
@@ -6,4 +8,8 @@ export async function updateLevels(memberId: string): Promise<void> {
 
     const randomExp = Math.floor(Math.random() * (15 - 5)) + 5;
     await upsertLevelEntry(memberId, randomExp);
+}
+
+export default function getCommands(): BotCommand[] {
+    return [new RankCommand()];
 }
