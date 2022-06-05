6ddd403b1faf39b275b186b568d943a11ef763c2
_ Lamar Wu <89696970+thenameiswu@users.noreply.github.com>
2022-06-05 12:10:05 -0500

    tweak(file resolver): tweaked deep file resolution

--- src/files.ts
+++ src/files.ts
@@ -1,37 +1,34 @@
-import path from "path";
-import fs from "fs";
+import fs from 'fs';
+import path from 'path';
 
-const walk = (
-    pathLike: fs.PathLike,
-    options?:
-        | {
-              encoding: BufferEncoding | null;
-          }
-        | BufferEncoding
-        | null
-        | undefined
-): string[] => {
-    let results: string[] = [];
-    const fileList = fs.readdirSync(pathLike, options);
-    for (let i = 0; i < fileList.length; i += 1) {
-        const file = fileList[i];
-        const stat = fs.statSync(path.join(pathLike.toString(), file));
-        results = [
-            ...results,
-            ...(stat && stat.isDirectory()
-                ? walk(path.join(pathLike.toString(), file))
-                : [file]),
-        ];
-    }
-    return results.map((filename) => path.join(pathLike.toString(), filename));
+/**
+ * A deep search function that resolves all files within a directory based on a list of extensions.
+ *
+ * @param {string} pathLike The base path to search through.
+ * @param {string[]} extensions The array of extensions to search for.
+ * @returns A list of paths for all the valid files within the given directory.
+ */
+
+export const resolveFiles = (pathLike: fs.PathLike, extensions: string[]): string[] => {
+	let results: string[] = [];
+
+	// collect directory contents
+	const fileList = fs.readdirSync(pathLike);
+
+	fileList.forEach((file: string, _) => {
+		const fileStats = fs.statSync(path.join(pathLike.toString(), file));
+
+		// if the content is a directory then we should re-run the function on it.
+		// otherwise we are going to add it to the results.
+		results = [...results, ...(fileStats && fileStats.isDirectory() ? resolveFiles(path.join(pathLike.toString(), file), extensions) : [file])];
+	});
+
+	// then we are going to loop through the results and resolve it's location to the current directory.
+	return results.map((filePath) => path.resolve(pathLike.toString(), filePath)).filter((path) => extensions.some((ext) => path.endsWith(ext)));
 };
 
-const commandFiles = walk(path.join(__dirname, "commands")).filter((file) =>
-    [".ts", ".js"].some((ext) => file.endsWith(ext))
-);
 
-const eventFiles = walk(path.join(__dirname, "events")).filter((file) =>
-    [".ts", ".js"].some((ext) => file.endsWith(ext))
-);
+const commandFiles = resolveFiles(path.join(__dirname, "commands"), [".ts", ".js"]);
+const eventFiles = resolveFiles(path.join(__dirname, "events"), [".ts", ".js"]);
 
 export { commandFiles, eventFiles };
