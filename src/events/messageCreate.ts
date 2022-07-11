e418523008b17aee73b6740cdf6464725d6812af
Dylan Hackworth <dylhack@outlook.com>
2022-07-11 12:55:00 -0500

    ref(messageCreate): split event duties into funcs

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -4,39 +4,45 @@ import { getSpecialChannel } from "../database";
 import { Bot } from "../structures";
 import { TypedEvent } from "../types";
 
+async function helpChan(message: Message): Promise<void> {
+    if (!message.content || message.author.bot || !message.guild) {
+        return;
+    }
+    const helpChannelOpt = await getSpecialChannel(
+        message.guild.id,
+        "help"
+    ).catch(() => null);
+    if (helpChannelOpt === null) {
+        return;
+    }
+    const helpChannel = helpChannelOpt as TextChannel;
+
+    const id = uuid().split("-");
+    const thId = id[id.length - 1];
+    const threadName = `Thread #${thId}`;
+
+    if (message.channel.id === helpChannel.id) {
+        message.startThread({
+            name: threadName,
+            autoArchiveDuration: "MAX",
+        });
+    }
+}
+
+async function massPingCheck(message: Message): Promise<void> {
+    if (
+        message.mentions.users.size > 5 &&
+        message.inGuild() &&
+        !message.member?.permissions.has("MENTION_EVERYONE")
+    ) {
+        await message.member?.timeout(600_000, "Mass mentions");
+    }
+}
+
 export default TypedEvent({
     eventName: "messageCreate",
     run: async (client: Bot, message: Message) => {
-        if (!message.content || message.author.bot || !message.guild) {
-            return;
-        }
-        let helpChannel;
-        try {
-            helpChannel = (await getSpecialChannel(
-                message.guild.id,
-                "help"
-            )) as TextChannel;
-        } catch (err) {
-            client.logger.console.error(err);
-            return;
-        }
-        const id = uuid().split("-");
-        const thId = id[id.length - 1];
-        const threadName = `Thread #${thId}`;
-
-        if (message.channel.id === helpChannel.id) {
-            message.startThread({
-                name: threadName,
-                autoArchiveDuration: "MAX",
-            });
-        }
-
-        if (
-            message.mentions.users.size > 5 &&
-            message.inGuild() &&
-            !message.member?.permissions.has("MENTION_EVERYONE")
-        ) {
-            await message.member?.timeout(600_000, "Mass mentions");
-        }
+        helpChan(message);
+        massPingCheck(message);
     },
 });
