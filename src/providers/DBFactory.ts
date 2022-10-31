5dc4fffd6d31445b7e0fd52c1a8e44016947ab7f
Dylan Hackworth <dylhack@gmail.com>
2022-10-31 01:26:51 -0500

    init prisma client

--- src/providers/DBFactory.ts
+++ src/providers/DBFactory.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 // TODO(dylhack): abstract client to nosql repositories
 export default class DBFactory {
-    private static client: PrismaClient;
+    private static client: PrismaClient | null = null;
 
     public static getClient(): PrismaClient {
+        if (this.client === null) this.client = new PrismaClient();
         return this.client;
     }
 }
